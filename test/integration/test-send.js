2e909284d7c9ca3a6260993fedf72f3e86353f63
Hans Brouwer <hans.brouwer@simacan.com>
2020-10-05 14:40:59 +0200

    fixed test-send.js

--- test/integration/test-send.js
+++ test/integration/test-send.js
@@ -16,7 +16,7 @@ server.listen(common.port, function() {
     }
   };
 
-  client.write(metrics, function(err) {
+  client.write(metrics, "baz.quz.", function(err) {
     assert.ok(!err);
 
     client.end();
@@ -25,19 +25,19 @@ server.listen(common.port, function() {
 
 process.on('exit', function() {
   var metric = server.metrics.shift();
-  assert.equal(metric.path, 'foo');
+  assert.equal(metric.path, 'baz.quz.foo');
   assert.equal(metric.value, 1);
   assert.ok(metric.timestamp + 1 >= Date.now() / 1000);
   assert.ok(metric.timestamp - 1 <= Date.now() / 1000);
 
   metric = server.metrics.shift();
-  assert.equal(metric.path, 'deep.down.a');
+  assert.equal(metric.path, 'baz.quz.deep.down.a');
   assert.equal(metric.value, 2);
   assert.ok(metric.timestamp + 1 >= Date.now() / 1000);
   assert.ok(metric.timestamp - 1 <= Date.now() / 1000);
 
   metric = server.metrics.shift();
-  assert.equal(metric.path, 'deep.down.b');
+  assert.equal(metric.path, 'baz.quz.deep.down.b');
   assert.equal(metric.value, 3);
   assert.ok(metric.timestamp + 1 >= Date.now() / 1000);
   assert.ok(metric.timestamp - 1 <= Date.now() / 1000);
